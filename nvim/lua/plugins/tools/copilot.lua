a182e16f20184cd140cb1e2bbc8bbf081dc02ea2
dvhsh <david@dvh.sh>
2025-02-21 15:24:14 -0800

    update nvim copilot config

--- nvim/lua/plugins/tools/copilot.lua
+++ nvim/lua/plugins/tools/copilot.lua
@@ -4,29 +4,21 @@ return {
 		cmd = "Copilot",
 		event = "InsertEnter",
 		config = function()
-			vim.g.copilot_no_tab_map = false
-			vim.g.copilot_assume_mapped = false
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
 			vim.g.copilot_tab_fallback = ""
 
-			local keymap = vim.keymap.set
-			keymap("i", "<Tab>", function()
-				if require("copilot.suggestion").is_visible() then
-					require("copilot.suggestion").accept()
-				else
-					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-				end
-			end, {
-				silent = true,
+			-- Use Ctrl+Tab for copilot accept
+			vim.keymap.set("i", "<C-Tab>", 'copilot#Accept("<CR>")', {
 				expr = true,
-				script = true,
 				replace_keycodes = false,
 			})
 
-			-- Additional keymaps (optional)
-			keymap("i", "<M-[>", "<Plug>(copilot-previous)", { silent = true })
-			keymap("i", "<M-]>", "<Plug>(copilot-next)", { silent = true })
-			keymap("i", "<M-\\>", "<Plug>(copilot-suggest)", { silent = true })
-			keymap("i", "<C-]>", "<Plug>(copilot-dismiss)", { silent = true })
+			-- Additional keymaps
+			vim.keymap.set("i", "<M-[>", "<Plug>(copilot-previous)", { silent = true })
+			vim.keymap.set("i", "<M-]>", "<Plug>(copilot-next)", { silent = true })
+			vim.keymap.set("i", "<M-\\>", "<Plug>(copilot-suggest)", { silent = true })
+			vim.keymap.set("i", "<C-]>", "<Plug>(copilot-dismiss)", { silent = true })
 		end,
 	},
 }
